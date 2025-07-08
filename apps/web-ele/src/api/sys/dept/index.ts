9d2c9b59c90284888a7caad70e4dab7ba1101d9f
henrikabel25 <henrikabel@foxmail.com>
2025-07-08 22:04:17 +0800

    feat: Add System Department

---
+++ apps/web-ele/src/api/sys/dept/index.ts
@@ -0,0 +1,17 @@
+import { requestClient } from '#/api/request';
+
+export async function getDeptPage(data: any) {
+  return requestClient.post('/sys/dept/index', data);
+}
+
+export async function createDept(data: any) {
+  return requestClient.post('/sys/dept/create', data);
+}
+
+export async function updateDept(data: any) {
+  return requestClient.put('/sys/dept/update', data);
+}
+
+export async function delDeptById(data: any) {
+  return requestClient.post('/sys/dept/delete', data);
+}
---
+++ apps/web-ele/src/views/sys/dept/index.vue
@@ -0,0 +1,138 @@
+<script lang="ts" setup>
+import type { VxeGridProps } from '#/adapter/vxe-table';
+
+import { ref } from 'vue';
+
+import { confirm, Page } from '@vben/common-ui';
+
+import { ElButton, ElMessage } from 'element-plus';
+
+import { useVbenVxeGrid } from '#/adapter/vxe-table';
+import { delDeptById, getDeptPage } from '#/api/sys/dept';
+
+import DeptForm from './form.vue';
+
+const deptFormRef = ref();
+
+interface RowType {
+  id: string;
+  parentId: null | number;
+  deptName: string;
+  deptStatus: string;
+}
+
+const gridOptions: VxeGridProps<RowType> = {
+  columns: [
+    {
+      field: 'deptName',
+      title: 'Department Name',
+      align: 'left',
+      treeNode: true,
+    },
+    { field: 'deptStatus', title: 'Status' },
+    {
+      field: 'action',
+      fixed: 'right',
+      slots: { default: 'action' },
+      title: 'Operation',
+      width: 120,
+    },
+  ],
+  exportConfig: {},
+  keepSource: true,
+  proxyConfig: {
+    ajax: {
+      query: async ({ page }) => {
+        return await getDeptPage({
+          pageNum: page.currentPage,
+          pageSize: page.pageSize,
+        });
+      },
+    },
+  },
+  pagerConfig: {
+    enabled: false,
+  },
+  editConfig: {
+    mode: 'row',
+    trigger: 'click',
+  },
+  treeConfig: {
+    parentField: 'parentId',
+    rowField: 'id',
+    transform: true,
+  },
+  toolbarConfig: {
+    custom: true,
+    export: true,
+    // import: true,
+    refresh: true,
+    zoom: true,
+  },
+};
+
+const [Grid, gridApi] = useVbenVxeGrid({
+  gridOptions,
+});
+
+const openForm = () => {
+  deptFormRef.value?.open();
+};
+
+const editRow = (row: RowType) => {
+  deptFormRef.value?.open(row);
+};
+
+const expandAll = () => {
+  gridApi.grid?.setAllTreeExpand(true);
+};
+
+const collapseAll = () => {
+  gridApi.grid?.setAllTreeExpand(false);
+};
+
+const deleteById = (row: RowType) => {
+  confirm({
+    content: 'Confirm deletion?',
+    icon: 'error',
+  }).then(async () => {
+    await delDeptById({
+      id: row.id,
+    })
+      .then(async () => {
+        await gridApi.reload();
+        ElMessage.success('Deletion successful');
+      })
+      .catch(() => {
+        ElMessage.error('Deletion failed');
+      });
+  });
+};
+</script>
+
+<template>
+  <Page>
+    <Grid>
+      <template #toolbar-tools>
+        <ElButton class="mr-2" bg text type="primary" @click="expandAll">
+          Expand All
+        </ElButton>
+        <ElButton type="primary" bg text @click="collapseAll">
+          Collapse All
+        </ElButton>
+      </template>
+      <template #toolbar-actions>
+        <ElButton class="mr-2" bg text type="primary" @click="openForm">
+          Add
+        </ElButton>
+      </template>
+      <template #action="{ row }">
+        <ElButton type="primary" link @click="editRow(row)"> Edit </ElButton>
+        <ElButton type="danger" link @click="deleteById(row)">
+          Delete
+        </ElButton>
+      </template>
+    </Grid>
+    <DeptForm ref="deptFormRef" :grid-api="gridApi" />
+  </Page>
+</template>
