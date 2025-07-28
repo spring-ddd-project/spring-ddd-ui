ed04947f246edc2551667330f8c2a889bd908fbb
henrikabel25 <henrikabel@foxmail.com>
2025-07-28 21:22:47 +0800

    fix: Add Table Display Fields Information

--- apps/web-ele/src/views/gen/info/form.vue
+++ apps/web-ele/src/views/gen/info/form.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import type { VxeTableGridOptions } from '#/adapter/vxe-table';
+
+import { reactive, ref } from 'vue';
 
 import { useVbenDrawer } from '@vben/common-ui';
 import { $t } from '@vben/locales';
@@ -7,9 +9,27 @@ import { $t } from '@vben/locales';
 import { ElCard, ElMessage } from 'element-plus';
 
 import { useVbenForm } from '#/adapter/form';
-import { getTableInfo } from '#/api/gen/table';
+import { useVbenVxeGrid } from '#/adapter/vxe-table';
+import { getTableInfo, getTablePage } from '#/api/gen/table';
 
 const writeForm = ref<Record<string, any>>({});
+const genTag = reactive([
+  {
+    color: 'processing',
+    label: $t('codegen.info.tag.attribute'),
+    value: 'attribute',
+  },
+  {
+    color: 'success',
+    label: $t('codegen.info.tag.table'),
+    value: 'table',
+  },
+  {
+    color: 'default',
+    label: $t('codegen.info.tag.form'),
+    value: 'form',
+  },
+]);
 
 const [Form, formApi] = useVbenForm({
   showDefaultActions: false,
@@ -62,62 +82,11 @@ const [Form, formApi] = useVbenForm({
   ],
 });
 
-const [ValueForm, valueFormApi] = useVbenForm({
-  showDefaultActions: false,
-  layout: 'horizontal',
-  commonConfig: {
-    componentProps: {
-      class: 'w-full',
-    },
-    colon: true,
-    labelWidth: 130,
-  },
-  submitOnChange: true,
-  schema: [
-    {
-      component: 'Input',
-      fieldName: 'propColumnName',
-      label: $t('codegen.info.propColumnName'),
-      componentProps: {
-        placeholder: `${$t('system.common.placeholder.input')} ${$t('codegen.info.propColumnName')}`,
-      },
-      rules: 'required',
-    },
-    {
-      component: 'Input',
-      fieldName: 'packageName',
-      label: $t('codegen.info.packageName'),
-      componentProps: {
-        placeholder: `${$t('system.common.placeholder.input')} ${$t('codegen.info.packageName')}`,
-      },
-      rules: 'required',
-    },
-    {
-      component: 'Input',
-      fieldName: 'className',
-      label: $t('codegen.info.className'),
-      componentProps: {
-        placeholder: `${$t('system.common.placeholder.input')} ${$t('codegen.info.className')}`,
-      },
-      rules: 'required',
-    },
-    {
-      component: 'Input',
-      fieldName: 'requestName',
-      label: $t('codegen.info.requestName'),
-      componentProps: {
-        placeholder: `${$t('system.common.placeholder.input')} ${$t('codegen.info.requestName')}`,
-      },
-      rules: 'required',
-    },
-  ],
-});
-
 const [Drawer, drawerApi] = useVbenDrawer({
   onOpenChange: async (open) => {
     if (open) {
       await getTableInfo(writeForm.value?.tableName).then((resp: any) => {
-        valueFormApi.setValues(resp);
+        // TODO valueFormApi.setValues(resp);
       });
     } else {
       drawerApi.setState({ loading: false });
@@ -152,6 +121,50 @@ const open = (row: any) => {
 const close = () => drawerApi.close();
 
 defineExpose({ open, close });
+
+const gridOptions: VxeTableGridOptions<any> = {
+  columns: [
+    { title: 'No.', type: 'seq', width: 50 },
+    { align: 'left', title: '#', type: 'checkbox', width: 50 },
+    {
+      field: 'tableName',
+      title: $t('codegen.table.tableName'),
+      align: 'left',
+    },
+    { field: 'tableComment', title: $t('codegen.table.tableComment') },
+    { field: 'createTime', title: $t('codegen.table.createTime') },
+    { field: 'tableCollation', title: $t('codegen.table.tableCollation') },
+    {
+      field: 'action',
+      fixed: 'right',
+      slots: { default: 'action' },
+      title: $t('system.common.operation'),
+      width: 100,
+    },
+  ],
+  exportConfig: {},
+  keepSource: true,
+  proxyConfig: {
+    ajax: {
+      query: async ({ page }) => {
+        return await getTablePage({
+          pageNum: page.currentPage,
+          pageSize: page.pageSize,
+        });
+      },
+    },
+  },
+  pagerConfig: {
+    enabled: false,
+  },
+  editConfig: {
+    mode: 'row',
+    trigger: 'click',
+  },
+};
+const [Grid, gridApi] = useVbenVxeGrid({
+  gridOptions,
+});
 </script>
 
 <template>
@@ -170,7 +183,7 @@ defineExpose({ open, close });
           <span>{{ $t('codegen.info.value') }}</span>
         </div>
       </template>
-      <ValueForm />
+      <Grid />
     </ElCard>
   </Drawer>
 </template>
