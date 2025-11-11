ab4b5708dda9dfc7070dc033811ff9ac904f157f
henrikabel25 <henrikabel@foxmail.com>
2025-11-11 21:44:09 +0800

    feat: add batch‑delete permission for data‑dictionary values

--- apps/web-ele/src/views/sys/dict/item/index.vue
+++ apps/web-ele/src/views/sys/dict/item/index.vue
@@ -183,7 +183,12 @@ defineExpose({ open, close });
           <ElButton type="primary" link @click="editRow(row)">
             {{ $t('system.common.button.edit') }}
           </ElButton>
-          <ElButton type="danger" link @click="deleteByIds(row)">
+          <ElButton
+            type="danger"
+            link
+            @click="deleteByIds(row)"
+            v-if="hasAccessByCodes(['sys:dict:item:delete'])"
+          >
             {{ $t('system.common.button.delete') }}
           </ElButton>
         </template>
